499e1a035fa5e417f495e0071ec213bc95d4ffda
MineBoxerSam <95152465+ss100249@users.noreply.github.com>
2021-12-04 00:17:21 -0800

    Update Chapter 1 MDP.py

--- Chapter 1 MDP.py
+++ Chapter 1 MDP.py
@@ -1,1 +1,14 @@
-
+'''import NumPy as ql'''
+	R = ql.matrix ([	[0,0,0,0,10,],
+				  [0,0,0,1,0,1],
+				  [0,0,100,1,0,0],
+				  [0,1,1,0,1,0],
+				  [1,0,0,1,0,0],
+					[0,1,0,0,0,0]	])
+	Q = ql.matrix(ql.zeros([6,6]))
+	
+	gamma = 0.8
+	
+	'''R is the reward matrix described in the mathematical analasys'''
+	
+	
